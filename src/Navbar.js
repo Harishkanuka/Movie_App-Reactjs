d0e6e24565b6cad4df27667ad999a537bb209854
Your Name <kanukaharish@gmail.com>
2023-05-28 19:10:01 +0530

    Movie App Using React

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,21 +1,68 @@
 import React from 'react';
+import styled from 'styled-components';
 
-class Navbar extends React.Component {
-	render() {
-		return (
-			<>
-				<div>
-					<div>Title</div>
-					<div>
-						<div>
-							<img src="" alt="Cart Icon" />
-							<span>0</span>
-						</div>
-					</div>
-				</div>
-			</>
-		);
+const Nav = styled.div`
+	height: 70px;
+	background: linear-gradient(170deg, #1bc059, #0d47a1);
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	position: relative;
+`;
+
+const Title = styled.div`
+	font-size: 30px;
+	color: #fff;
+	font-weight: 600;
+	font-family: 'Times New Roman', Times, serif;
+	text-transform: uppercase;
+	margin-left: 20px;
+	cursor: pointer;
+	&:hover {
+		color: #153569;
 	}
+`;
+
+const CartImg = styled.img`
+	height: 48px;
+	margin-right: 20px;
+`;
+
+const CartIconContainer = styled.div`
+	position: relative;
+	cursor: pointer;
+`;
+
+const CartCount = styled.div`
+	background: ${(props) => props.color};
+	border-radius: 50%;
+	padding: 4px 8px;
+	position: absolute;
+	right: 10px;
+	top: -5px;
+	font-size: 12px;
+	visibility: ${(props) => (props.show ? 'visible' : 'hidden')};
+`;
+
+function Navbar(props) {
+	const { cartCount } = props;
+	return (
+		<>
+			<Nav>
+				<Title>MOVIE APP</Title>
+
+				<CartIconContainer>
+					<CartImg
+						alt="Cart-Icon"
+						src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
+					/>
+					<CartCount color="yellow" show={true}>
+						{cartCount}
+					</CartCount>
+				</CartIconContainer>
+			</Nav>
+		</>
+	);
 }
 
 export default Navbar;
